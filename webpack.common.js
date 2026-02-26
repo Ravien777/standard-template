d98f5fcbca7ae26027e857c9b5e7bc5b133e67a3
Ravien777 <raviensewpal@gmail.com>
2026-02-26 16:13:27 -0300

    webpack merge

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,33 +1,7 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-export default {
+export default merge(common, {
   mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|webp)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+  devtool: "source-map",
+});
